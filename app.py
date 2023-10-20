da883168027cd6169f6b5b5a60abb278c67ae20e
Vijaiey88 <vijaiey88@gmail.com>
2023-10-20 07:17:40 +0530

    changes

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import json
 import pickle
+import bz2file as bz2
 from flask import Flask,request,app,jsonify,render_template
 import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, 'rb')
+    data = pickle.load(data)
+    return data
+regmodel = decompress_pickle('copper_reg_model.pbz2')
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -17,7 +22,7 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=np.array(list(data.values())).reshape(1,-1)
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
@@ -25,10 +30,10 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    final_input=np.array(data).reshape(1,-1)
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    return render_template("home.html",prediction_text="The Copper selling price is {}".format(output**2))
 
 
 
